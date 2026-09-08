a1b56a161ae348d3829be4d96a350810686f06a6
liupeng <603277298@qq.com>
2026-09-08 17:17:48 +0800

    feat(linapro-demo-dynamic): 删除无用的demo

--- linapro-demo-dynamic/plugin_embed.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "embed"
-
-// EmbeddedFiles contains the plugin manifest, hosted frontend assets, and
-// convention-based SQL resources used to generate runtime snapshot sections.
-//
-//go:embed plugin.yaml frontend manifest
-var EmbeddedFiles embed.FS
